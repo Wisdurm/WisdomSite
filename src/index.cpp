47b036b7a3216b603649426bd86be225161b2b9f
Wisdurm <luukas.kola@gmail.com>
2025-08-09 17:30:58 +0300

    Template and ssl removal

--- src/index.cpp
+++ src/index.cpp
@@ -4,8 +4,11 @@ int main()
 {
     crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/")([](){
-        return "Hello world";
+    CROW_ROUTE(app, "/")([]
+        (const crow::request& req){
+	    auto page = crow::mustache::load("begin.html");
+        crow::mustache::context ctx({{"ip", req.remote_ip_address}});
+	    return page.render(ctx);
     });
 
     app.port(18080).multithreaded().run();
