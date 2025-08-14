1f0d7645c6683c91580675d15f201bbc09afaa86
Wisdurm <luukas.kola@gmail.com>
2025-08-14 19:58:25 +0300

    Fun and totally wont result in horrible things :D

--- src/index.cpp
+++ src/index.cpp
@@ -1,15 +1,32 @@
 #include "crow.h"
+#include <string>
+
+std::string process_text(std::string str)
+{
+    if (str.length() > 100)
+        str.erase(100);
+    return str;
+}
 
 int main()
 {
     crow::SimpleApp app;
+    std::string text = "Bruh";
 
-    CROW_ROUTE(app, "/")([]
+    CROW_ROUTE(app, "/")([&text]
         (const crow::request& req){
 	    auto page = crow::mustache::load("begin.html");
-        crow::mustache::context ctx({{"ip", req.remote_ip_address}});
+        crow::mustache::context ctx({{"msg", text}});
+        text += "a";
 	    return page.render(ctx);
     });
+
+    CROW_ROUTE(app, "/send")
+	    .methods("GET"_method, "POST"_method)([&text](const crow::request& req, crow::response& res){
+		text = process_text(req.url_params.get("msg"));
+        res.redirect("/");
+        res.end();
+	});
 
     app.port(18080).multithreaded().run();
 }
