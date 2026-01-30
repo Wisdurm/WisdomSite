02f9b4965a4547f5ed6f9a84564187023cd49780
Wisdurm <luukas.kola@gmail.com>
2026-01-30 20:33:23 +0200

    More work. Not sure when I'm gonna finish this :)

--- static/components/header.js
+++ static/components/header.js
@@ -1,67 +1,7 @@
 // Ye shall be warned, I've spent HOURS on this with NEY ANY progress. Do tread lightly...
 
-const headerTemplate = document.createElement('template');
-headerTemplate.innerHTML = `
-    <style>
-        @font-face{
-            font-family: 'FreeSerif';
-            src: url(FreeSerif.otf);
-        }
-        .sub:hover {
-            box-shadow: inset 0 -2px 0 0 #346734;
-        }
-        a {
-            font-weight: 700;
-            color: #346734;
-            text-decoration: none;
-            font-size: 1.2em;
-            font-family: FreeSerif;
-        }
-        nav {
-            height: 50px;
-            display:flex;
-        }
-
-        .logo{
-            font-size: 3.1em;
-            vertical-align:bottom;
-        }
-
-        .link{
-            margin: auto 15px 0 15px;
-        }
-
-        .sub{
-            font-family: Lucida Console;
-            font-size: 1.2em;
-        }
-
-        .right{
-            display: inline-block;
-            width:100%;
-            text-align:right;
-        }
-
-        #daily:hover{
-            box-shadow: none;
-        }
-
-        header{
-            border-bottom: 3px solid #346734;
-            padding-bottom: 5px;
-        }
-    </style>
-    <header>
-    <nav>
-        <a href="/" class="link logo">Wisdurm.fi</a>
-        <a href="/projects" class="link sub">Projects</a>
-        <a href="/contact" class="link sub">Contact</a>
-        <a href="/blog" class="link sub">Blog</a>
-        <a class="link sub right" id="daily">Word(s) of the day: <slot name="wisdom">Bruhh</slot></a>
-    </nav>
-    </header>
-`;
-
+// Fear not, for the olden times are long gone now, and an era of light
+// shall spread across the lands..
 
 class Header extends HTMLElement {
   constructor() {
@@ -69,8 +9,21 @@ class Header extends HTMLElement {
   }
 
   connectedCallback() {
-    const shadowRoot = this.attachShadow({mode : "closed" });
-    shadowRoot.appendChild(headerTemplate.content);
+			this.innerHTML = `
+    <header>
+    <nav>
+        <a href="/" class="link logo">Wisdurm.fi</a>
+        <a href="/projects" class="sub">Projects</a>
+        <a href="/contact" class="sub">Contact</a>
+        <a href="/blog" class="sub">Blog</a>
+    </nav>
+    <div class="flex-right" id="daily-container">
+        <p id="daily">
+          Word(s) of the day: <slot name="wisdom">Bruhh</slot>
+        </p>
+    </div>
+    </header>
+`;
   }
 }
 
